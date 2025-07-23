6f2af84c48cbaa002ab45cdd6a4c5e99edb29ace
SoyWater <ivanlxy123@gmail.com>
2025-07-23 17:37:29 +0800

    Fix lint warning

--- backend/src/db/types/dbtypes.ts
+++ backend/src/db/types/dbtypes.ts
@@ -1,6 +1,5 @@
 import { BuildOptions, Model, Sequelize } from "sequelize";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type DB = Record<string, hasAssociateAndInit>;
 
 export type DbModelStatic<T extends Model> = typeof Model<T> & {
